384fc1f101eda58f39e9e6b7c95c5820e8e880b2
Aitor <helloimaitor@gmail.com>
2019-09-03 22:07:54 +0100

    add functions structure

--- math-motion.js
+++ math-motion.js
@@ -1,9 +1,30 @@
 export default (p) => {
+  function play() {
+    p.loop()
+  }
+
+  function stop() {
+    p.noLoop()
+  }
+
+  function reset() {
+
+  }
+
   function setup() {
+    p.createCanvas(p.windowWidth, p.windowHeight)
+    p.frameRate(24)
+
+    reset()
   }
 
   function draw() {
   }
+
+  p.play = play
+  p.stop = stop
+
+  p.reset = reset
 
   p.setup = setup
   p.draw = draw
